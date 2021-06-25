7ea1c679acc0a883f814f2f58601df1dc75ec390
felipe cembranelli <feliperc@ciandt.com>
2021-06-25 16:41:09 -0400

    adding matrix 2 D

--- datastructures.tests/MatrixTest.cs
+++ datastructures.tests/MatrixTest.cs
@@ -7,19 +7,55 @@ namespace datastructure.tests
     public class MatrixTest
     {
         [Fact]
-        public void Create_Matrix_Should_Return_Elements()
+        public void Create_One_Dimension_Matrix_Should_Return_Correct_Bounderies()
         {
             // arrange
-            const int SIZE = 2;
+            const int dimension = 0;        // zero based dimension
 
             int[] values = {1,2,3,4};
             // act
-            var arr = Matrix.CreateMatrix(SIZE, values);
+            var arr = Matrix.Create1DMatrix(values);
 
             // assert
-            Assert.Equal(3, arr.GetUpperBound(1));
-            Assert.Equal(3, arr.GetUpperBound(2));
+            Assert.Equal(0, arr.GetLowerBound(dimension));  // return index of lower bound
+            Assert.Equal(3, arr.GetUpperBound(dimension));  // return index of upper bound
         }
+        [Fact]
+        public void Create_2_Dimension_Matrix()
+        {
+            // arrange 4 X 2
+
+            int[,] values = {
+                                {1,1},
+                                {2,2},
+                                {3,3},
+                                {4,4}
+                            };
+
+            // act
+            var arr = Matrix.Create2DMatrix(values);
+
+            // assert
+            Assert.True(true);
+        }        
+
+        [Fact]
+        public void Create_2B_Dimension_Matrix()
+        {
+            // arrange 4 X 3
+            int[,] values = {
+                                {1,1,1},
+                                {2,2,2},
+                                {3,3,3},
+                                {4,4,4}
+                            };
+
+            // act
+            var arr = Matrix.Create2DMatrix(values);
+
+            // assert
+            Assert.True(true);
+        }        
     }
 }
 
--- datastructures/Matrix/Matrix.cs
+++ datastructures/Matrix/Matrix.cs
@@ -4,7 +4,7 @@ namespace MyDataStructure.Matrix
 {
     public class Matrix
     {
-        public static int[] CreateMatrixOneDimension(int[] values)
+        public static int[] Create1DMatrix(int[] values)
         {
             {
                 int i, j;
@@ -25,43 +25,43 @@ namespace MyDataStructure.Matrix
                 Console.Write("\nThe matrix is : \n");
                 for (i = 0; i < size; i++)
                 {
-                    Console.Write("\n");
-                    for (j = 0; j < size; j++)
-                        Console.Write("{0}\t", arr1[i]);
+                    Console.Write("{0}\t", arr1[i]);
                 }
                 Console.Write("\n\n");
 
                 return arr1;
             }
         }
-        public static int[,] CreateMatrix(int size, int[] values)
+        public static int[,] Create2DMatrix(int[,] values)
         {
             {
                 int i, j;
                 int count = 0;
-                int[,] arr1 = new int[size, size];
+                int numberRows = values.GetLength(0);
+                int numberColumns = values.GetLength(1);
 
-                Console.Write("\n\nRead a 2D array of size " + size.ToString() + "x" + size.ToString() + " and print the matrix :\n");
+                int[,] arr1 = new int[numberRows, numberColumns];
+
+                Console.Write("\n\nRead a 2D array of size " + numberRows.ToString() + "x" + numberColumns.ToString() + " and print the matrix :\n");
                 Console.Write("------------------------------------------------------\n");
 
                 /* Stored values into the array*/
                 Console.Write("Input elements in the matrix :\n");
-                for (i = 0; i < size; i++)
+                for (i = 0; i < numberRows; i++)
                 {
-                    for (j = 0; j < size; j++)
+                    for (j = 0; j < numberColumns; j++)
                     {
                         Console.Write("element - [{0},{1}] : ", i, j);
-                        //arr1[i, j] = Convert.ToInt32(Console.ReadLine());
-                        arr1[i, j] = values[count];
+                        arr1[i, j] = values[i,j];
                         count++;
                     }
                 }
 
                 Console.Write("\nThe matrix is : \n");
-                for (i = 0; i < size; i++)
+                for (i = 0; i < numberRows; i++)
                 {
                     Console.Write("\n");
-                    for (j = 0; j < size; j++)
+                    for (j = 0; j < numberColumns; j++)
                         Console.Write("{0}\t", arr1[i, j]);
                 }
                 Console.Write("\n\n");
