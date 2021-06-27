f8e94ec100c306b9eb51399240b0d35ab6d05f96
felipe cembranelli <feliperc@ciandt.com>
2021-06-27 15:58:19 -0400

    adding stack class

--- datastructures/Stack/min-stack.cs
+++ datastructures/Stack/min-stack.cs
@@ -2,27 +2,20 @@ using System.Collections;
 
 public class MinStack {
 
-    // /** initialize your data structure here. */
-    // private Stack stack = new Stack();
+    /** initialize your data structure here. */
+    private Stack stack = new Stack();
 
 
-    // public MinStack() {
+    public MinStack() {
         
-    // }
+    }
     
-    // public void Push(int val) {
-    //     stack.Push(val);
-    // }
+    public void Push(int val) {
+        stack.Push(val);
+    }
     
-    // public void Pop() {
-        
-    // }
+    public void Pop() {
+        stack.Pop();
+    }
     
-    // public int Top() {
-        
-    // }
-    
-    // public int GetMin() {
-        
-    // }
 }
